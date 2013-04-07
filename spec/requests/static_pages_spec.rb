520cedafe2f02494dfe1abb23178da8cb12e5cbe
jiazom <jiazom@126.com>
2013-04-07 14:22:17 +0800

    Make a basic User model (include secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,32 +5,32 @@ describe "StaticPages" do
     # page about the home page
     describe "Home page" do
       before { visit root_path }
-      it { page.should have_selector('h1', text: "Sample App") }
-      it { page.should have_selector("title", text: full_title('')) }
-      it {page.should have_selector("title",text: "| Home")}
+      it { should have_selector('h1', text: "Sample App") }
+      it { should have_selector("title", text: full_title('')) }
+      it { should have_selector("title",text: "| Home")}
     end
 
     #page about the contact page
     describe "Contact page" do
       before { visit contact_path }
-      it { page.should have_selector('h1', text: "Contact") }
-      it { page.should have_selector("title", text: full_title('Contact')) }
-      it { page.should have_selector("title", text: "| Contact") }
+      it { should have_selector('h1', text: "Contact") }
+      it { should have_selector("title", text: full_title('Contact')) }
+      it { should have_selector("title", text: "| Contact") }
     end
 
     #page about the help page
     describe "Help page" do
       before { visit help_path }
-      it { page.should have_selector('h1', text: "Help") }
-      it { page.should have_selector("title", text: full_title('Help')) }
-      it { page.should have_selector("title", text: "| Help") }
+      it { should have_selector('h1', text: "Help") }
+      it { should have_selector("title", text: full_title('Help')) }
+      it { should have_selector("title", text: "| Help") }
     end
 
     #page about the about page
     describe "About page" do
       before { visit about_path }
-      it { page.should have_selector('h1', text: "About Us") }
-      it { page.should have_selector("title", text: full_title('About Us')) }
-      it { page.should have_selector("title", text: "| About Us")}
+      it { should have_selector('h1', text: "About Us") }
+      it { should have_selector("title", text: full_title('About Us')) }
+      it { should have_selector("title", text: "| About Us")}
     end
 end
